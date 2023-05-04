13ac1af70368b5388106515fb44ef5432e05b7ce
unknown <danirizky704@gmail.com>
2023-05-04 21:57:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home  from "./pages/Home";
+import Indonesia  from "./pages/Indonesia";
+import Covid19 from "./pages/Covid19";
+import Programming from "./pages/Programming";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/indonesia" element={<Indonesia/>} />
+        <Route path="/programming" element={<Programming/>} />
+        <Route path="/covid19" element={<Covid19/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Covid19.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Covid19 = () => {
+  return (
+    <div>Covid-19</div>
+  )
+}
+
+export default Covid19
